48f5f62056bc9d0fe0a016f7311c4588962e75e3
sokurii <122415844+sokurii@users.noreply.github.com>
2023-04-04 08:49:41 +0900

    230404 수학

---
+++ BOJ/[BOJ]_4153_직각삼각형.py
@@ -0,0 +1,11 @@
+while True:
+    lst = list(map(int,input().split()))
+    lst.sort()
+    if sum(lst)==0:
+        break
+
+    if lst[0]**2 + lst[1]**2 == lst[2]**2:
+        print('right')
+    else:
+        print('wrong')
+
